08fbb6e06e5429f3b2fc80a1670919589770f319
César Guerra <warderer@gmail.com>
2023-05-30 21:35:41 -0600

    ✨ Formulario con useForm

--- 01-react-forms/src/pages/FormHook.jsx
+++ 01-react-forms/src/pages/FormHook.jsx
@@ -1,28 +1,48 @@
 import logo from '../assets/react.svg'
+import useForm from '../hooks/useForm'
 
 const FormHook = () => {
+  // Paso #1: Creo con un objeto con los valores iniciales de cada input
+  const datos = {
+    nombre: '',
+    apellido: '',
+    otro: '',
+    edad: 0,
+    genero: '',
+    email: '',
+    password: ''
+  }
+
+  // Paso #2 Voy a crear la función que se ve a a ejecutar al enviar el formulario con mi lógica de negocio
+  const sendData = (data) => {
+    console.log('La información que enviaste:', data)
+  }
+
+  // Paso #3: Usar el custom hook de useForm
+  const { input, handleSubmit, handleInputChange } = useForm(sendData, datos)
+
   return (
     <div className='login'>
       <div className='login-container'>
         <img src={logo} alt='logo' />
         <form
-          onSubmit={() => {}/* HANDLE SUBMIT */}
+          onSubmit={handleSubmit}
           style={{ display: 'flex', flexDirection: 'column' }}
         >
           <label htmlFor='nombre'>Nombre</label>
-          <input type='text' name='nombre' placeholder='Tu nombre' id='nombre' value='' onChange={() => {}/* HANDLE INPUT */} />
+          <input type='text' name='nombre' placeholder='Tu nombre' id='nombre' value={input.nombre} onChange={handleInputChange} />
 
           <label htmlFor='apellido'>Apellido</label>
-          <input type='text' name='apellido' placeholder='Tu Apellido' id='apellido' value='' onChange={() => {}/* HANDLE INPUT */} />
+          <input type='text' name='apellido' placeholder='Tu Apellido' id='apellido' value={input.apellido} onChange={handleInputChange} />
 
           <label htmlFor='otro'>Otro</label>
-          <input type='text' name='otro' placeholder='Tu Otro' id='otro' value='' onChange={() => {}/* HANDLE INPUT */} />
+          <input type='text' name='otro' placeholder='Tu Otro' id='otro' value={input.otro} onChange={handleInputChange} />
 
           <label htmlFor='edad'>Edad</label>
-          <input type='number' name='edad' placeholder='Tu edad' id='edad' value='' onChange={() => {}/* HANDLE INPUT */} />
+          <input type='number' name='edad' placeholder='Tu edad' id='edad' value={input.edad} onChange={handleInputChange} />
 
           <label htmlFor='genero'>Genero</label>
-          <select name='genero' id='genero' value='' onChange={() => {}/* HANDLE INPUT */}>
+          <select name='genero' id='genero' value={input.genero} onChange={handleInputChange}>
             <option value=''>Elige un genero</option>
             <option value='M'>Masculino</option>
             <option value='F'>Femenino</option>
@@ -35,8 +55,8 @@ const FormHook = () => {
             name='email'
             placeholder='correo@mail.com'
             id='email'
-            value=''
-            onChange={() => {}/* HANDLE INPUT */}
+            value={input.email}
+            onChange={handleInputChange}
           />
 
           <label htmlFor='password'>Password</label>
@@ -44,8 +64,8 @@ const FormHook = () => {
             type='password'
             name='password'
             id='password'
-            value=''
-            onChange={() => {}/* HANDLE INPUT */}
+            value={input.password}
+            onChange={handleInputChange}
           />
           <button type='submit'>
             Iniciar Sesion
