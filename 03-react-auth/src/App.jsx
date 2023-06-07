6c1e13d2ce4be434f71695114b65d729c8c550f1
César Guerra <warderer@gmail.com>
2023-06-06 21:06:07 -0600

    ✨ Header con BEM pero sin estilos

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
+import Header from '@/components/Header'
 
 function App () {
   return (
     <>
+      <Header />
       <h1>React Auth</h1>
       <Home />
       <Dashboard />
