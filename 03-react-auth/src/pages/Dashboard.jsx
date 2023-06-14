f7abee2e2c8194a36ea8858424622dcc4012c240
César Guerra <warderer@gmail.com>
2023-06-13 21:06:53 -0600

    ✨ Dashboard con datos del usuario

--- 03-react-auth/src/pages/Dashboard.jsx
+++ 03-react-auth/src/pages/Dashboard.jsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from 'react'
+import { getSingleUserService } from '@/services/userService'
+import { useAuthContext } from '@/hooks/useAuth'
+
 const Dashboard = () => {
+  const { userPayload } = useAuthContext()
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getSingleUserService(userPayload.id)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error:', error.message)
+      }
+    }
+    fetchUserData()
+  }, [userPayload.id])
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      {
+        userData?.first_name && <p>First Name: {userData.first_name}</p>
+      }
+      {
+        userData?.last_name && <p>Last Name: {userData.last_name}</p>
+      }
+      {
+        userData?.gender && <p>Gender: {userData.gender}</p>
+      }
+      {
+        userData?.email && <p>Email: {userData.email}</p>
+      }
+    </>
   )
 }
 export default Dashboard
--- 03-react-auth/src/services/userService.js
+++ 03-react-auth/src/services/userService.js
@@ -4,8 +4,10 @@ const BASE_URL = 'http://localhost:3000'
 
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getSingleUserService = (id) => axios.get(`${BASE_URL}/users/${id}`)
 
 export {
   registerUserService,
-  loginUserService
+  loginUserService,
+  getSingleUserService
 }
