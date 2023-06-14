28c3c5a3cba030ff09052b5f3df8bc37054759fd
César Guerra <warderer@gmail.com>
2023-06-13 19:45:25 -0600

    ✨ useAuth hook

---
+++ 03-react-auth/src/hooks/useAuth.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
+
+// Paso 3. Crear un hook para usar el contexto
+export const useAuthContext = () => {
+  const context = useContext(AuthContext)
+
+  if (context === undefined) {
+    throw new Error('useAuthContext debe ser usado dentro de AuthProvider')
+  }
+
+  return context
+}
