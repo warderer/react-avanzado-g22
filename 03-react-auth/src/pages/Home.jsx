a41399ba3bc960cabd3a83d11f4006e405bb03f5
César Guerra <warderer@gmail.com>
2023-06-15 23:49:59 -0600

    ♻️ Loading para ItemsData en Home

--- 03-react-auth/src/pages/Home.jsx
+++ 03-react-auth/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { getAllItems } from '@/services/itemServices'
 
 const Home = () => {
   const [itemsData, setItemsData] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchItemData = async () => {
@@ -10,6 +11,7 @@ const Home = () => {
         const response = await getAllItems()
         if (response.status === 200) {
           setItemsData(response.data)
+          setLoading(false)
         }
       } catch (error) {
         console.log('Ocurrio un error:', error.message)
@@ -23,17 +25,21 @@ const Home = () => {
       <h1>Home</h1>
       <div className='d-flex flex-row flex-wrap justify-content-center'>
         {/* Si itemsData no esta vacio, recorro el arreglo con Map y creo un Card de Bootstrap para cada elemento */}
-        {itemsData && itemsData?.map((product) => (
-          <div className='card' style={{ width: '18rem' }} key={product.id}>
-            <img className='card-img-top' style={{ maxHeight: '300px' }} src={product.image} alt={product.product_name} />
-            <div className='card-body'>
-              <h5 className='card-title'>{product.product_name}</h5>
-              <p className='card-text'>{product.description}</p>
-              {/* Aqui no se implementa el botón, pero basta con sustituir "a" por Link de react-router-dom y la ruta del enlace indicar el componente que mostrará la información de un solo producto, seguido del id del producto */}
-              <a href='#' className='btn btn-primary'>Comprar</a>
+        {
+        loading
+          ? <h1>Cargando...</h1>
+          : itemsData.map((product) => (
+            <div className='card' style={{ width: '18rem' }} key={product.id}>
+              <img className='card-img-top' style={{ maxHeight: '300px' }} src={product.image} alt={product.product_name} />
+              <div className='card-body'>
+                <h5 className='card-title'>{product.product_name}</h5>
+                <p className='card-text'>{product.description}</p>
+                {/* Aqui no se implementa el botón, pero basta con sustituir "a" por Link de react-router-dom y la ruta del enlace indicar el componente que mostrará la información de un solo producto, seguido del id del producto */}
+                <a href='#' className='btn btn-primary'>Comprar</a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+}
       </div>
     </>
   )
